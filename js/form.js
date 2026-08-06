a9505232ca936e1e2f9a6d0c2be54c478e710b68
CailanAbade <cailanabademoreiraa@gmail.com>
2026-08-05 23:48:33 -0300

    alteração contatos 1

--- js/form.js
+++ js/form.js
@@ -2,10 +2,21 @@
 //class contato
 
 class contato {
+
+    constructor(nome,sobrenome,email,cpf,telefone,contato){
+        this.nome = nome
+        this.sobrenome = sobrenome
+        this.email = email
+        this.cpf = cpf
+        this.telefone = telefone
+        this.contato = contato
+    }
     
 }
 
 function Post(form) {
+
+    event.preventDefault()
 
   let data = new contato(form.elements.namedItem("nome").value,
             form.elements.namedItem("sobrenome").value, 
@@ -13,8 +24,11 @@ function Post(form) {
             form.elements.namedItem("cpf").value, 
             form.elements.namedItem("telefone").value, 
             form.elements.namedItem("contato").value);
+
+console.log(data);
   
 }
+
 
 function Enviar() {
 
